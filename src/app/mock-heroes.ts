d14fb918e2d23f23ba42de6a1429bbaa82cd9311
jakkappro <jakkappro@gmail.com>
2022-01-21 10:53:36 +0100

    5.2

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,15 @@
 import {Hero} from './hero';
+import { ITEMS } from './mock-items';
 
 export const HEROES: Hero[] = [
-    { id: 11, name: 'Kubik' },
-    { id: 12, name: 'Zeri' },
-    { id: 13, name: 'MrRobot' },
-    { id: 14, name: 'SirUstrelPrdel' },
-    { id: 15, name: 'Ratatui' },
-    { id: 16, name: 'MacbookMan' },
-    { id: 17, name: 'MissMILF' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Toreador' },
-    { id: 20, name: 'Hrusky' }
+    { id: 11, name: 'Kubik', items: [ITEMS[1], ITEMS[2]] },
+    { id: 12, name: 'Zeri', items: [ITEMS[2]] },
+    { id: 13, name: 'MrRobot', items: [ITEMS[3]] },
+    { id: 14, name: 'SirUstrelPrdel', items: [ITEMS[4]] },
+    { id: 15, name: 'Ratatui', items: [ITEMS[5]] },
+    { id: 16, name: 'MacbookMan', items: [ITEMS[6]] },
+    { id: 17, name: 'MissMILF', items: [ITEMS[7]] },
+    { id: 18, name: 'Dr IQ', items: [ITEMS[8]] },
+    { id: 19, name: 'Toreador', items: [ITEMS[9]] },
+    { id: 20, name: 'Hrusky', items: [ITEMS[10]] }
   ];
